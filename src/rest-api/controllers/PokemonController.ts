c2ef4ec5bbf960310cc3fb0f3d7ec2f6831a5592
Sophie Bushchak <sophie.bushchak@hva.nl>
2020-12-24 18:10:48 +0100

    Pokedex endpoint with pagination

--- src/rest-api/controllers/PokemonController.ts
+++ src/rest-api/controllers/PokemonController.ts
@@ -1,14 +1,41 @@
-import {getConnection} from "typeorm";
+import {createConnection, getConnection, Repository} from "typeorm";
 import {Pokemon} from "../../database/entities/Pokemon";
 
 
 export default class PokemonController {
 
-    private pokemonRepository = getConnection().getRepository(Pokemon)
+    private pokemonRepository: Repository<Pokemon>;
+
+    constructor() {
+        createConnection()
+            .then(connection => {
+                this.pokemonRepository = connection.getRepository(Pokemon)
+            })
+    }
 
     getPokedex = async (req, res, next) => {
-        const offset = req.query.offset;
-        const limit = req.query.limit;
-        res.status(200).json({message: "Okay"})
+        try {
+            let offset: number = req.query.offset;
+            let limit: number = req.query.limit;
+            if (!offset || !limit) {
+                offset = 0;
+                limit = 20;
+            }
+            const pokemon = await this.pokemonRepository.find({
+                skip: offset,
+                take: limit
+            })
+            return res.status(200).json(
+                {
+                    message: `Found ${pokemon.length} Pokémon.`,
+                    statusCode: 200,
+                    pokemon: pokemon
+                })
+        } catch (errorThrown) {
+            const error = {
+                errorThrown
+            }
+            next(error)
+        }
     }
 }
