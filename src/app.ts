99f318e5510675adcf713c726693abc3eb13c02c
Sophie <sophie.bushchak@hva.nl>
2024-09-24 17:09:09 +0200

    csrf

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 // @ts-ignore
 import helmet from "helmet";
+import { doubleCsrf} from "csrf-csrf";
 
 import Config from "../config";
 import {performance} from "perf_hooks"
@@ -12,7 +13,27 @@ import helloRoutes from "./rest-api/routes/HelloRoutes";
 const app = express();
 app.use(helmet());
 
+const {generateToken, doubleCsrfProtection} = doubleCsrf({
+    getSecret: () => "Secret", // A function that optionally takes the request and returns a secret
+    getSessionIdentifier: (req) => "", // A function that should return the session identifier for a given request
+    cookieName: "__Host-psifi.x-csrf-token", // The name of the cookie to be used, recommend using Host prefix.
+    cookieOptions: {
+        sameSite: "lax",  // Recommend you make this strict if posible
+        path: "/",
+        secure: true,
+    },
+    size: 64, // The size of the generated tokens in bits
+    ignoredMethods: ["HEAD", "OPTIONS"], // A list of request methods that will not be protected.
+    getTokenFromRequest: (req) => req.headers["x-csrf-token"], // A function that returns the token from the request
+});
+
 const port = Config.PORT;
+
+app.get('/csrf-token', (req, res) => {
+    const csrfToken = generateToken(req, res);
+    // You could also pass the token into the context of a HTML response.
+    res.json({ csrfToken });
+})
 
 app.use((req, res, next) => {
     console.log(`${req.method} ${req.originalUrl}`)
